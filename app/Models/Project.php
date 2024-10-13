6f8d7ad03ba12c748f6b1a68562d48f8f2af0123
AlessioConsolo <alessioconsolo3@gmail.com>
2024-10-13 16:45:18 +0200

    [UPD] Added function technologies to project.php and created "create project technology table"

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -23,5 +23,10 @@ class Project extends Model
         return $this->belongsTo(Type::class);
     }
     
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+
     public $timestamps = true;
 }
